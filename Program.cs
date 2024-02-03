f9fac7948d90364f252e14f746d5fb3152744ee4
Laptop <Laptop@DESKTOP-AAIMPLJ>
2024-02-03 18:29:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ejercicio6
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el sueldo bruto:");
+        double sueldoBruto = Convert.ToDouble(Console.ReadLine());
+
+        double aumento = 10;
+
+        double sueldoFinal = AplicarAumento(sueldoBruto, aumento);
+
+        Console.WriteLine("El sueldo final es: " + sueldoFinal);
+    }
+
+    static double AplicarAumento(double sueldoBruto, double aumento)
+    {
+        double aumentoMonetario = (sueldoBruto * aumento) / 100;
+        return sueldoBruto + aumentoMonetario;
+    }
+}
